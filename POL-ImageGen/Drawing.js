2ee1e5a177774f26eab95264515b19b3243c97ba
stc <stc@binaura.net>
2018-10-11 18:22:16 +0200

    improved POL gfx

--- POL-ImageGen/Drawing.js
+++ POL-ImageGen/Drawing.js
@@ -1,36 +1,57 @@
-let name1 = "Robi";
-let name2 = "Dani";
+let name1 = "Alma";
+let name2 = "Dinnye";
 let date = "2018. 10. 10"
 let  hash = "0xfccf2af2aa0b01177d0d99997bdc22c711d4fbd83769946f9866750f466d2549";
 
 var s = function( p ) {
 	let imgHeart = p.loadImage("data/heart.png");
+	let imgSciss = p.loadImage("data/sciss.png");
 	let font = p.loadFont("data/Inconsolata-Regular.ttf");
-	let imgOrigami = p.loadImage("data/origami.png");
+	let imgOrigami = p.loadImage("data/heartorig.png");
+	let hash1 = hash.slice(0,hash.length/2);
+	let hash2 = hash.slice(hash.length/2);
+	
 	p.setup = function() {
-		let c = p.createCanvas(1200,700);
+		let c = p.createCanvas(1050,1485);
 		c.parent("Drawing");
 		p.textFont(font);
 	}
 	p.draw = function() {
 		p.background(240);
+		p.noFill();
+		p.push();
+		p.stroke(0,200);
+		p.translate(p.width/2,p.height/2);
+		p.rotate(p.radians(45));
+		p.translate(-p.width/2,-p.height/2);
+		p.rectMode(p.CENTER);
+		p.rect(p.width/2,p.height/2,650,650);
+		p.pop();
 		p.fill(0,180);
 		p.textSize(40);
 		p.textAlign(p.RIGHT, p.CENTER);
-		p.text(name1, p.width/2 - 60, p.height/4);
+		p.text(name1, p.width/2 - 60, p.height/3);
 		p.textAlign(p.LEFT, p.CENTER);
-		p.text(name2, p.width/2 + 60, p.height/4);
-		p.imageMode(p.CENTER,p.CENTER);
-		p.image(imgHeart,p.width/2,p.height/4,100,100);
-		p.textAlign(p.CENTER, p.CENTER);
-		p.textSize(20);
-		p.text("Carved forever in the Blockchain since " + date + " at", p.width/2,p.height/3);
-		p.textSize(30);
-		p.text(hash, p.width/2,p.height/3 + 30);
+		p.text(name2, p.width/2 + 60, p.height/3);
+		p.imageMode(p.CENTER);
+		p.image(imgHeart,p.width/2,p.height/3.5,100,100);
 
+		p.push();
+		p.rotate(-p.radians(90));
+		p.textAlign(p.CENTER, p.CENTER);
 		p.textSize(14);
-		p.text("Print this page out & fold it according to the instructions below \n & give it to your love", p.width/2,p.height- p.height/2);
-		p.image(imgOrigami,p.width/2,p.height-p.height/3, imgOrigami.width/3,imgOrigami.height/3);
+		p.text(hash1, -p.width/1.5,p.width/3.5);
+		p.pop();
+
+		p.push();
+		p.rotate(p.radians(90));
+		p.textAlign(p.CENTER, p.CENTER);
+		p.textSize(14);
+		p.text(hash2, p.width/1.5, -(p.width - p.width/3.5));
+		p.pop();
+
+		p.image(imgOrigami,p.width/2,p.height-p.height/8, imgOrigami.width/2,imgOrigami.height/2);
+		p.image(imgSciss, p.width - p.width/4,p.height-p.height/2.74,imgSciss.width/4,imgSciss.height/4);
 
 	}
 }
